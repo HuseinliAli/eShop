e459b598196f004f5b7a3f402f765e5c50b2efe5
HuseinliAli <ali.guseynoov@gmail.com>
2023-02-08 00:59:26 +0400

    v2.5.13

---
+++ eShop.webui/Data/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using eShop.webui.Models;
+
+namespace eShop.webui.Data
+{
+    public class CategoryRepository
+    {
+        private static List<Category> _categories = null;
+
+        static CategoryRepository()
+        {
+            _categories = new List<Category>()
+            {
+                new Category{CategoryId=1,Name = "Phones", Description="Category of Phones"},
+                new Category{CategoryId=2,Name = "TVs", Description="Category of TVs"},
+                new Category{CategoryId=3,Name = "Noutbooks", Description="Category of Noutbooks"} 
+            };
+        }
+
+        public static List<Category> Categories { get { return _categories; } }
+
+        public static void AddProduct(Category category)
+        {
+            _categories.Add(category);
+        }
+
+        public static Category GetCategoryById(int id)
+        {
+            return _categories.FirstOrDefault(p => p.CategoryId == id);
+        }
+    }
+}
---
+++ eShop.webui/Data/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using eShop.webui.Models;
+
+namespace eShop.webui.Data
+{
+    public static class ProductRepository
+    {
+        private static List<Product> _products = null;
+
+        static ProductRepository()
+        {
+            _products = new List<Product>()
+            {
+                 new Product{ProductId = 1,Name = "Samsung",Price = 2345.5,Description = "good price", ImageUrl="1.jpg"},
+                 new Product{ProductId = 2,Name = "Iphone",Price = 5434.5,Description = "good price", ImageUrl="1.jpg"},
+                 new Product{ProductId = 3,Name = "Xiaomi",Price = 768.5,Description = "good price", ImageUrl = "1.jpg"},
+                 new Product{ProductId = 4, Name = "Poco",Price = 567.5,Description = "good price", ImageUrl = "1.jpg"}
+            };
+        }
+
+        public static List<Product> Products { get { return _products; } }
+
+        public static void AddProduct(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public static Product GetProductById(int id)
+        {
+            return _products.FirstOrDefault(p => p.ProductId == id);
+        }
+    }
+}
